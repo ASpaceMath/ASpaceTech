0b9ad1c75ba1950b1f6c873c23372b06b01ad3cf
Raziel Melchor <razielmelchor@gmail.com>
2023-09-22 20:17:08 -0700

    remove vite config changes

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src",
-  server: {
-    proxy: {
-      "/api/v1/": "http://localhost:5000/",
-    },
-  },
 });
