4452af012968d1fe5cf4ad2a39dffc2dcfc04362
Raziel Melchor <razielmelchor@gmail.com>
2023-09-23 22:49:49 -0700

    add proxy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src",
+  server: {
+    proxy: {
+      "/api/health": {
+        target: "http://localhost:80",
+        secure: false,
+      },
+    },
+  },
 });
