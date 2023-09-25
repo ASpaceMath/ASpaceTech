b1249751a993a2c0b445ea7999b8242605e3e4d5
Raziel Melchor <razielmelchor@gmail.com>
2023-09-24 17:01:16 -0700

    remove api key

--- src/ExpressionLoader.jsx
+++ src/ExpressionLoader.jsx
@@ -39,7 +39,7 @@ const ExpressionLoader = () => {
           method: "POST",
           headers: {
             "content-type": "application/json",
-            app_key: "c1a441f711d68478ff8292e7b98ca7a555891fc6012b175ce2ef11618edeeb87"
+            app_key: "API KEY!"
           },
           body: JSON.stringify({
             src: img,
